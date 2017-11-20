6a7bbb0e3fbc52b83e47c744b4480d400a732fc0
Artiya Thinkumpang <artya4u@gmail.com>
2017-11-20 21:43:01 +0700

    Remove the useless cfonts and fix code style.

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@
 const program = require('commander');
 const axios = require('axios');
 const ora = require('ora');
-const cfonts = require('cfonts');
 const Table = require('cli-table2');
-const colors = require('colors');
 
 program
   .version('0.0.6')
@@ -13,10 +11,16 @@ program
   .option('-t, --top [index]', 'Show the top coins ranked from 1 - [index] according to the market cap', null)
   .parse(process.argv);
 
-const find = program.find
-const top = !isNaN(program.top) && +program.top > 0 ? +program.top : (find ? 1500 : 10)
-const convert = program.convert.toUpperCase()
-const availableCurrencies = ['USD', 'AUD', 'BRL', 'CAD', 'CHF', 'CLP', 'CNY', 'CZK', 'DKK', 'EUR', 'GBP', 'HKD', 'HUF', 'IDR', 'ILS', 'INR', 'JPY', 'KRW', 'MXN', 'MYR', 'NOK', 'NZD', 'PHP', 'PKR', 'PLN', 'RUB', 'SEK', 'SGD', 'THB', 'TRY', 'TWD', 'ZAR']
+const find = program.find;
+const top = !isNaN(program.top) && +program.top > 0 ? +program.top : (find ? 1500 : 10);
+const convert = program.convert.toUpperCase();
+const availableCurrencies = ['USD', 'AUD', 'BRL', 'CAD', 'CHF',
+  'CLP', 'CNY', 'CZK', 'DKK', 'EUR',
+  'GBP', 'HKD', 'HUF', 'IDR', 'ILS',
+  'INR', 'JPY', 'KRW', 'MXN', 'MYR',
+  'NOK', 'NZD', 'PHP', 'PKR', 'PLN',
+  'RUB', 'SEK', 'SGD', 'THB', 'TRY',
+  'TWD', 'ZAR'];
 if (availableCurrencies.indexOf(convert) === -1) {
   return console.log('We cannot convert to your fiat currency.'.red)
 }
@@ -31,8 +35,8 @@ const table = new Table({
     'bottom-left': '-',
     'bottom-right': '-',
     'left': '║',
-    'left-mid': '-' ,
-    'mid': '-' ,
+    'left-mid': '-',
+    'mid': '-',
     'mid-mid': '-',
     'right': '║',
     'right-mid': '-',
@@ -42,50 +46,40 @@ const table = new Table({
   colWidths: [6, 14, 15, 15, 15, 20]
 });
 
-cfonts.say('coinmon', {
-  font: 'simple3d',
-  align: 'left',
-  colors: ['yellow'],
-  background: 'Black',
-  letterSpacing: 2,
-  lineHeight: 1,
-  space: true,
-  maxLength: '0'
-});
 const spinner = ora('Loading data').start();
-const sourceUrl = `https://api.coinmarketcap.com/v1/ticker/?limit=${top}&convert=${convert}`
+const sourceUrl = `https://api.coinmarketcap.com/v1/ticker/?limit=${top}&convert=${convert}`;
 axios.get(sourceUrl)
-.then(function (response) {
-  spinner.stop();
-  console.log(`Data sourced from coinmarketcap.com at ${new Date().toLocaleTimeString()}`)
-  response.data
-    .filter(record => {
-      if (find) {
-        const keyword = `${find}`
-        return record.symbol.toLowerCase().indexOf(keyword) !== -1 || record.name.toLowerCase().indexOf(keyword) !== -1
-      }
-      return true
-    })
-    .map(record => {
-      const percentChange24h = record.percent_change_24h
-      const textChange24h = `${percentChange24h}%`
-      const change24h = percentChange24h? (percentChange24h > 0 ? textChange24h.green : textChange24h.red) : 'NA'
-      const percentChange1h = record.percent_change_1h
-      const textChange1h = `${percentChange1h}%`
-      const change1h = percentChange1h ? (percentChange1h > 0 ? textChange1h.green : textChange1h.red) : 'NA'
-      return [
-        record.rank,
-        `💰  ${record.symbol}`, 
-        record[`price_${convert}`.toLowerCase()], 
-        change24h,
-        change1h,
-        record[`market_cap_${convert}`.toLowerCase()]
-      ]
-    })
-    .forEach(record => table.push(record))
-  console.log(table.toString());
-})
-.catch(function (error) {
-  spinner.stop();
-  console.error('Coinmon is not working now. Please try again later.'.red);
-});
+  .then(function (response) {
+    spinner.stop();
+    console.log(`Data sourced from coinmarketcap.com at ${new Date().toLocaleTimeString()}`);
+    response.data
+      .filter(record => {
+        if (find) {
+          const keyword = `${find}`;
+          return record.symbol.toLowerCase().indexOf(keyword) !== -1 || record.name.toLowerCase().indexOf(keyword) !== -1
+        }
+        return true
+      })
+      .map(record => {
+        const percentChange24h = record.percent_change_24h;
+        const textChange24h = `${percentChange24h}%`;
+        const change24h = percentChange24h ? (percentChange24h > 0 ? textChange24h.green : textChange24h.red) : 'NA';
+        const percentChange1h = record.percent_change_1h;
+        const textChange1h = `${percentChange1h}%`;
+        const change1h = percentChange1h ? (percentChange1h > 0 ? textChange1h.green : textChange1h.red) : 'NA';
+        return [
+          record.rank,
+          `💰  ${record.symbol}`,
+          record[`price_${convert}`.toLowerCase()],
+          change24h,
+          change1h,
+          record[`market_cap_${convert}`.toLowerCase()]
+        ]
+      })
+      .forEach(record => table.push(record));
+    console.log(table.toString());
+  })
+  .catch(function (error) {
+    spinner.stop();
+    console.error('Coinmon is not working now. Please try again later.'.red);
+  });
